388ebba4b9dfa51445bdc4f04d19ec02eae40ec8
Itay <eizzo77@gmail.com>
2021-08-30 22:38:46 +0300

    hey

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "mongodb+srv://ItayZaguri:togZzih3VqZBqthG@cluster0.3gnvx.mongodb.net/Game-Data?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("connected to DB"));
+mongoose.connect(
+  "mongodb+srv://ItayZaguri:togZzih3VqZBqthG@cluster0.3gnvx.mongodb.net/Game-Data?retryWrites=true&w=majority",
+  (err) => {
+    if (err) throw err;
+    console.log("connected to MongoDB");
+  }
+);
